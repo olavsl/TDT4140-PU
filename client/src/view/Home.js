ed2e0631524869c5543a71fb37cc46852f963649
Olav Selnes Lorentzen <olavslo@stud.ntnu.no>
2023-02-08 12:34:47 +0100

    #1feat: Added Feed space

--- client/src/view/Home.js
+++ client/src/view/Home.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header"
 import Filter from "../components/Filter"
+import Feed from "../components/Feed"
 
 const Home = () => {
     return (
         <div className="home">
             <Header />
             <Filter />
+            <Feed />
         </div>
     )
 }
