61792a65f64b4af7d0c2cfcaf643379dfeaae948
tuvagn <tuvagn@stud.ntnu.no>
2023-02-09 14:44:47 +0100

    #2feat: Added login and signup button with styling.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,15 @@
 const Header = () => {
     return (
         <div className="header">
-            <h1 className="heading">Peregrinate</h1>
+            <div>
+                <h1 className="heading">Peregrinate</h1>
+            </div>
+            <div> 
+                <button className="header-button" id="login-button">Log in</button>
+            </div>
+            <div> 
+                <button className="header-button" id="signup-button">Sign up</button>
+            </div>
         </div>
     )
 }
