91791e589baa6268ed0a68a045bf9b6b414fe3b8
erlenelo <erlenelo@stud.ntnu.no>
2023-02-10 16:47:57 +0100

    #11fix: remove unnecessary button for dialog

--- client/src/components/TravelCard.js
+++ client/src/components/TravelCard.js
@@ -131,7 +131,6 @@ export const TravelCard = ({ travel }) => {
                                     </Typography>
                                 </Box>
                             </Grid>
-                            <Button> Expand </Button>
                         </CardContent>
                     </CardActionArea>
                 </Card>
