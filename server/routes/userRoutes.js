016481d40ff2586504e31bbdbcc76eec15d3388e
olavsl <olav.selnes.lorentzen@hotmail.com>
2023-02-11 12:46:26 +0100

    #13refactor: Changed a few file names to be more descriptive

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const User = require("../schema/userModel")
+const User = require("../schema/userSchema")
 
 const router = express.Router()
 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const express = require("express")
 const mongoose = require("mongoose")
-const userRoutes = require("./routes/users")
+const userRoutes = require("./routes/userRoutes")
 
 // Suppress warning
 mongoose.set('strictQuery', true);
