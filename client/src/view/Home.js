39a4171636189732e3bed6fd628a90dff730d528
IVER SKIBSRUD <iverksk@stud.ntnu.no>
2023-02-12 16:05:23 +0100

    #5feat: Created login form with styling. Co.auth.: Olav

--- client/src/view/Home.js
+++ client/src/view/Home.js
@@ -2,14 +2,17 @@ import Header from "../components/Header"
 import Filter from "../components/Filter"
 import Feed from "../components/Feed"
 import Ads from "../components/Ads"
+import Loginform from "../components/Loginform"
 
 const Home = () => {
     return (
         <div className="home">
+            <Loginform />
             <Header />
             <Filter />
             <Feed />
             <Ads />
+            
         </div>
     )
 }
