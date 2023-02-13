ff09681e5fa95090ddee75860f870dab0e426471
IVER SKIBSRUD <iverksk@stud.ntnu.no>
2023-02-13 12:14:09 +0100

    #5feat: finisehd loginform design Co.Auth.: Erlend

--- client/src/components/Loginform.js
+++ client/src/components/Loginform.js
@@ -4,13 +4,13 @@ return (
         <h1 className="loginHeading">Log in</h1>
         <input type="text" className="loginInput" id="usernameInput" placeholder="Username"></input>
         <input type="password" className="loginInput" id="passwordInput" placeholder="Password"></input>
-        {/* <hr className="notALine"></hr> */}
-        <button className="button" id="loginButton">Log in</button>
+        <hr className="notALine"></hr>
+        <button className="formButton" id="loginButton" onClick="login()">Log in</button>
         <p className="dontHaveAnAccount">Don't have an account?</p>
-        <button className="button" id="signupButton">Sign up</button>
+        <button className="formButton" id="signupButton">Sign up</button>
+        <script src="test.js"></script>
     </form>
-
+    
 )
 }
-
 export default Loginform
