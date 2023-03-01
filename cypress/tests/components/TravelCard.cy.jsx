8411b1ebb4179e453943ab8898a039a8057f868b
andrinekirstine <andrine.kirstine@gmail.com>
2023-03-01 01:12:35 +0100

    #29 feat: fixes test for travelcard

--- cypress/tests/components/TravelCard.cy.jsx
+++ cypress/tests/components/TravelCard.cy.jsx
@@ -7,3 +7,34 @@ describe('<TravelCard />', () => {
     cy.mount(<TravelCard />)
   })
 })
+
+describe('TravelCard', () => {
+  //Get contact with the server
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  //Get the info we need to test
+  beforeEach(() => {
+    cy.get('travelTitle')
+    cy.get('travelShortDesc')
+    cy.get('travelStartEnd')
+    cy.get('travelDistance')
+    cy.get('travelAuthor')
+    cy.get('travelType')
+    cy.get('travelCountry')
+    cy.get('travelPrice')
+  })
+
+  //Check if it contains the right info
+  it("should successfully get a travel item", () => {
+    cy.contains('travelTitle')
+    cy.contains('travelShortDesc')
+    cy.contains('travelStartEnd')
+    cy.contains('travelDistance')
+    cy.contains('travelAuthor')
+    cy.contains('travelType')
+    cy.contains('travelCountry')
+    cy.contains('travelPrice')
+  })
+})
--- cypress/tests/components/TravelCardExpanded.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import TravelCardExpanded from '../TravelCardExpanded'
-import '../../support/commands'
-
-describe('<TravelCardExpanded />', () => {
-  it('renders', () => {
-    cy.mount(<TravelCardExpanded />)
-  })
-})
