cecd2ac5c3b547d0c534736f3c3583c310c2c28e
IVER SKIBSRUD <iverksk@stud.ntnu.no>
2023-03-02 10:48:40 +0100

    #27 feat: added tabs for feed Co.Auth.: Erlend

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -6,9 +6,20 @@ import { useTravelsContext } from "../hooks/useTravelsContext";
 const Feed = () => {
     const { travels, dispatch } = useTravelsContext()
     const [addNewTravel, setAddNewTravel] = useState(false) 
+    const [activeIndex, setActiveIndex] = useState(1);
+    // const handleClick = (index) => setActiveIndex;
+    // const checkActive = (index, className) => activeIndex === index ? className: "";
 
     const fireAddNewTravel = () => {
         setAddNewTravel(current => !current)
+    }
+
+    const [toggleValue, setToggleValue] = useState(true);
+    const toggleRecent = () => {
+        setToggleValue(true);
+    }
+    const toggleTopList = () => {
+        setToggleValue(false);
     }
 
     useEffect(() => {
@@ -24,130 +35,58 @@ const Feed = () => {
         fetchTravels()
     }, [])
 
-    //data is dummy data until backend data is up and running.
-
-    const data = [
-        {
-            id: 1,
-            title: "Trip",
-            country: "Norway",
-            start: "Lindesnes",
-            end: "Nordkapp",
-            travelType: "Hike",
-            distance: 1000,
-            desc: "Palms sweaty, knees weak, arms are heavy, there's vomit on his sweater already, mom's spaghetti",
-            author: "User Userson",
-            price: 200,
-        },
-        {
-            id: 2,
-            title: "Trip2",
-            country: "Norway",
-            start: "Lindesnes",
-            end: "Nordkapp",
-            travelType: "Hike",
-            distance: 1000,
-            desc: "Palms sweaty, knees weak, arms are heavy, there's vomit on his sweater already, mom's spaghetti",
-            author: "User Userson",
-            price: 200,
-        },
-        {
-            id: 3,
-            title: "Trip3",
-            country: "Norway",
-            start: "Lindesnes",
-            end: "Nordkapp",
-            travelType: "Hike",
-            distance: 1000,
-            desc: "Palms sweaty, knees weak, arms are heavy, there's vomit on his sweater already, mom's spaghetti",
-            author: "User Userson",
-            price: 200,
-        },
-        {
-            id: 4,
-            title: "Trip4",
-            country: "Norway",
-            start: "Lindesnes",
-            end: "Nordkapp",
-            travelType: "Hike",
-            distance: 1000,
-            desc: "Palms sweaty, knees weak, arms are heavy, there's vomit on his sweater already, mom's spaghetti",
-            author: "User Userson",
-            price: 200,
-        },
-        {
-            id: 5,
-            title: "Trip5",
-            country: "Norway",
-            start: "Lindesnes",
-            end: "Nordkapp",
-            travelType: "Hike",
-            distance: 1000,
-            desc: "Palms sweaty, knees weak, arms are heavy, there's vomit on his sweater already, mom's spaghetti",
-            author: "User Userson",
-            price: 200,
-        },
-        {
-            id: 6,
-            title: "Trip6",
-            country: "Norway",
-            start: "Lindesnes",
-            end: "Nordkapp",
-            travelType: "Hike",
-            distance: 1000,
-            desc: "Palms sweaty, knees weak, arms are heavy, there's vomit on his sweater already, mom's spaghetti",
-            author: "User Userson",
-            price: 200,
-        },
-        {
-            id: 7,
-            title: "Trip7",
-            country: "Norway",
-            start: "Lindesnes",
-            end: "Nordkapp",
-            travelType: "Hike",
-            distance: 1000,
-            desc: "Palms sweaty, knees weak, arms are heavy, there's vomit on his sweater already, mom's spaghetti",
-            author: "User Userson",
-            price: 200,
-        },
-        {
-            id: 8,
-            title: "Trip8",
-            country: "Norway",
-            start: "Lindesnes",
-            end: "Nordkapp",
-            travelType: "Hike",
-            distance: 1000,
-            desc: "Palms sweaty, knees weak, arms are heavy, there's vomit on his sweater already, mom's spaghetti",
-            author: "User Userson",
-            price: 200,
-        },
-        
-
-    ]
-
     return (
         <div className="feed">
-            {/* Maps all data (currently dummy data) into TravelCard components, listing them in the feed.*/}
-            {/* {data.map( travel => (
-                <TravelCard key={travel.id} travel={travel} />
-            ))} */}
 
-            {travels && travels.map((travel) => (
-                <TravelCard key={travel._id} travel = {travel} /*title={travel.title} country={travel.country}
+            <div className="feedHeader">
+                <button className="tabButton" id="recentButton" onClick={toggleRecent}>Recent</button>
+                <button className="tabButton" id="toplistButton" onClick={toggleTopList}>Toplist</button>
+            </div>
+            {toggleValue ? <div className='Tab' id='Recent'>
+                {travels && travels.map((travel) => (
+                    <TravelCard key={travel._id} travel = {travel} /*title={travel.title} country={travel.country}
                     start={travel.startDestination} end={travel.endDestination} price={travel.price} 
                     travelType={travel.travelType}*/ />
-            ))}
+                ))}
 
-            <button className="add-travel-button" onClick={fireAddNewTravel}>
-                <div className="add-travel-plus-sign-component" id="vertical-plus-sign-component" />
-                <div className="add-travel-plus-sign-component" id="horizontal-plus-sign-component" />
-            </button>
 
-            {addNewTravel && 
-                <AddTravelForm className="add-travel-form" />
-            }
+                <button className="add-travel-button" onClick={fireAddNewTravel}>
+                    <div className="add-travel-plus-sign-component" id="vertical-plus-sign-component" />
+                    <div className="add-travel-plus-sign-component" id="horizontal-plus-sign-component" />
+                </button>
+
+                {addNewTravel && 
+                    <AddTravelForm className="add-travel-form" />
+                }
+            </div>
+            :
+            <div></div> }
+
+
+            <div className='Tab' id='Toplist'>
+                Hello World
+                {!toggleValue ? <div className='Tab' id='Toplist'>
+                {travels && travels.map((travel) => (
+                    <TravelCard key={travel._id} travel = {travel} /*title={travel.title} country={travel.country}
+                    start={travel.startDestination} end={travel.endDestination} price={travel.price} 
+                    travelType={travel.travelType}*/ />
+                ))}
+
+
+                <button className="add-travel-button" onClick={fireAddNewTravel}>
+                    <div className="add-travel-plus-sign-component" id="vertical-plus-sign-component" />
+                    <div className="add-travel-plus-sign-component" id="horizontal-plus-sign-component" />
+                </button>
+
+                {addNewTravel && 
+                    <AddTravelForm className="add-travel-form" />
+                }
+            </div>
+            :
+            <div></div> }
+            </div>
+
+            
         </div>
     )
 }
