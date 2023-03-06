0dc90486e389b3f4fed417521d1dd99a108018d3
IVER SKIBSRUD <iverksk@stud.ntnu.no>
2023-03-06 11:32:30 +0100

    #34 feat: add search bar

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,7 +1,9 @@
 const Filter = () => {
     return (
         <div className="filter">
-
+            <div>
+                <input className="searchBar" onKeyUp="search_trip()" name="search" type="text" placeholder='Search trips' />
+            </div>
         </div>
     )
 }
