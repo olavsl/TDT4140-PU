59428e9244eaaf02ace10c46c4e275ab40e02999
andrinekirstine <andrine.kirstine@gmail.com>
2023-03-08 10:07:10 +0100

    #24 fix: updates actions and state -> comments

--- client/src/context/CommentContext.js
+++ client/src/context/CommentContext.js
@@ -2,26 +2,41 @@ import { createContext, useReducer, useMemo } from "react"
 
 export const CommentContext = createContext()
 
-export const commentReducer = (state, action) => {
+export const commentReducer = (comments, action) => {
     switch (action.type) {
         case "GET_COMMENTS":{
             return {
                 comment: action.payload
             }}
         case "CREATE_COMMENT":
-            return {
-                comment: [...state.comment, action.payload]}
+            return [...comments, {
+                commentID: action.commentID,
+                author: action.author,
+                travelRoute: action.travelID,
+                text: action.text,
+                time: action.time
+            }]
+        case "UPDATE_COMMENT": 
+            return comments.map((t) => {
+                if (t.author === action.comment.author && t.travelID === action.comment.travelID) {
+                  return action.comment
+                } else {
+                  return t
+                }
+            });
+        case "DELETE_COMMENT":
+            return comments.filter((t) => t.id !== action.id);
         default: 
             throw Error ("Uknown action:"+ action.type)
     } 
 }
 
 export const CommentContextSupplier = ({ children }) => {
-    const [state, dispatch] = useReducer(commentReducer, {
+    const [comments, dispatch] = useReducer(commentReducer, {
         comment: null
     })
 
-    const valueComCon = useMemo(() => ({...state, dispatch}), [state, dispatch])
+    const valueComCon = useMemo(() => ({...comments, dispatch}), [comments, dispatch])
 
     return (
         <CommentContext.Provider value={valueComCon}>
